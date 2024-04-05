c311197d2a210bb2013f9fe1cd8827b772583c6a
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2024-04-05 17:26:24 -0400

    Update build_deb.sh

--- build_deb.sh
+++ build_deb.sh
@@ -3,5 +3,4 @@ rm -rf /tmp/babeld
 mkdir /tmp/babeld
 cp -R root/* /tmp/babeld
 cp -R DEBIAN /tmp/babeld
-#dpkg-deb --no-uniform-compression --build /tmp/babeld
-dpkg-deb --build /tmp/babeld
+dpkg-deb -Z none -b /tmp/babeld
